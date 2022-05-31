874745f57b530be04d66f2ea2f060d88739ae082
JoshuaOloton <olotonjoshua@gmail.com>
2022-05-31 15:15:40 +0000

    corrected numbering error

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 print("{}{}".format(0, 1), end='')
-for i in range(10):
+for i in range(0, 10):
     for j in range(i+1, 10):
+        if (i == 0 and j == 1):
+            continue
         print(", {}{}".format(i, j), end='')
 print("".format())
