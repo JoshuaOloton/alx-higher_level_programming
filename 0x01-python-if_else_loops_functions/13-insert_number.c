a028adc6efed9a05588098177dc6802025430c03
JoshuaOloton <olotonjoshua@gmail.com>
2022-06-01 23:52:04 +0000

    corrections to task 13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -16,6 +16,12 @@ listint_t *insert_node(listint_t **head, int number)
 	if (new == NULL)
 		return (NULL);
 	new->n = number;
+	if ((*head) == NULL)
+	{
+		new->next = NULL;
+		*head = new;
+		return (*head);
+	}
 	if ((*head)->n >= number)
 	{
 		new->next = *head;
