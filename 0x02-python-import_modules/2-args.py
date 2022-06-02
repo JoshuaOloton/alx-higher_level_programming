ac490fa53882a0386e77891e0792a852b54db276
JoshuaOloton <olotonjoshua@gmail.com>
2022-06-02 13:17:57 +0000

    added task 3

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    n = len(sys.argv)
+    if n == 1:
+        print("{} arguments.".format(0))
+        sys.exit()
+    print("{} argument:".format(1)) if n == 2 else print("{} arguments:".format(n - 1))
+    for i in range(1, n):
+        print("{}: {}".format(i, sys.argv[i]))
+
