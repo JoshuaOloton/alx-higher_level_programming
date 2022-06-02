abd0a2f94a90eabc47b22ff5439e02beca40ee5d
JoshuaOloton <olotonjoshua@gmail.com>
2022-06-02 13:20:28 +0000

    pycodestyle check for task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if n == 1:
         print("{} arguments.".format(0))
         sys.exit()
-    print("{} argument:".format(1)) if n == 2 else print("{} arguments:".format(n - 1))
+    print("{} argument:".format(1)) if n == 2\
+        else print("{} arguments:".format(n - 1))
     for i in range(1, n):
         print("{}: {}".format(i, sys.argv[i]))
-
