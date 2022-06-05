9b75f89e54e59d23017f6eed5403835f9be07c91
JoshuaOloton <olotonjoshua@gmail.com>
2022-06-05 21:33:49 +0000

    removed import in task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-import copy
-
-
 def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
     if idx < 0 or idx >= len(my_list):
-        return my_list.copy()
-    newlist = copy.deepcopy(my_list)
+        return new_list
     newlist[idx] = element
     return newlist
