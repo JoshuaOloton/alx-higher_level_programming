2713ce7c6dec3f59a2104ee581d9e4b4e4d0cde5
JoshuaOloton <olotonjoshua@gmail.com>
2022-06-08 22:11:08 +0000

    added task 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    
+    new_list = []
+    for i, j in zip(set_1, set_2):
+        if i == j:
+            new_list.append(i)
+    return new_list
