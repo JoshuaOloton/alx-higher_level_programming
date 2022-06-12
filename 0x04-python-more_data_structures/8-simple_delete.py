0b5df698b36dbc8337b2e7c661964b7b5f5b2362
JoshuaOloton <olotonjoshua@gmail.com>
2022-06-12 12:53:06 +0000

    added task

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    new_dict = {}
-    for k, v in a_dictionary:
-        if k != key:
-            new_dict[k] = v
-    return new_dict
+    if a_dictionary.get(key):
+        del a_dictionary[key]
+    return a_dictionary
