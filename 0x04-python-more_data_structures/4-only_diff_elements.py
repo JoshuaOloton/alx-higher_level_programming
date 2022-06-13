3dadc7ab169bce367e6fa2452ea27b0a74065d71
JoshuaOloton <olotonjoshua@gmail.com>
2022-06-13 23:16:50 +0000

    fixed task 4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    my_list = []
-    if len(set_1) <= len(set_2):
-        small_set = set_1
-        big_set = set_2
-    else:
-        small_set = set_2
-        big_set = set_1
-    for i in big_set:
-        if i not in small_set:
-            my_list.append(i)
-    return my_list
+    new_list = []
+    my_list = list(set_1) + list(set_2)
+    my_set = set(my_list)
+    for i in my_set:
+        if my_list.count(i) == 1:
+            new_list.append(i)
+    return new_list
