2dc91228fa47e074579bf505c53f27de00d73cdc
JoshuaOloton <olotonjoshua@gmail.com>
2022-06-13 23:06:22 +0000

    corrected task 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    my_list = []
-    if len(set_1) <= len(set_2):
-        small_set = set_1
-        big_set = set_2
-    else:
-        small_set = set_2
-        big_set = set_1
-    for i in small_set:
-        if i in big_set:
-            my_list.append(i)
-    return my_list
+    new_list = []
+    my_list = list(set_1) + list(set_2)
+    my_set = set(my_list)
+    for i in my_set:
+        if my_list.count(i) == 2:
+            new_list.append(i)
+    return new_list
