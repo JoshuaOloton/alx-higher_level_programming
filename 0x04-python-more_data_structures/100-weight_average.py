5112279b19f4231e175b29f75e0e052ecc48275e
JoshuaOloton <olotonjoshua@gmail.com>
2022-06-13 06:51:29 +0000

    added task 100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,3 +2,6 @@
 def weight_average(my_list=[]):
     if len(my_list) == 0:
         return 0
+    weighted_sums = list(map(lambda x: x[0] * x[1], my_list))
+    denum = [i[1] for i in my_list]
+    return sum(weighted_sums)/sum(denum)
