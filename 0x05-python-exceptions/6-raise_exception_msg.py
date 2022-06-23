949c81868e81e8c6341332cb036b08a52f931ca0
JoshuaOloton <olotonjoshua@gmail.com>
2022-06-23 13:43:39 +0000

    added task 6

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
-    """ function that raises a type exception. """
+    """ function that raises a name exception with a message. """
     raise NameError(message)
