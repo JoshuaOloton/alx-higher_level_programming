2248994b85522302a3cd33bde6532b5c1f06b794
JoshuaOloton <olotonjoshua@gmail.com>
2022-07-04 14:50:14 +0000

    added task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
-""" defines a class MyList that inherits from list """ 
+""" defines a class MyList that inherits from list """
+
 
 class MyList(list):
-    def __init__(self):
-        super().__init__()
+    def print_sorted(self):
+        self.__new_list = self[:]
+        self.__new_list.sort()
+        print(self.__new_list)
