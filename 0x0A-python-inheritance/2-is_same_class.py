864ad012c21cf30a454a53cd09247d9f35bbb633
JoshuaOloton <olotonjoshua@gmail.com>
2022-07-04 15:02:04 +0000

    added task 1

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-""" defines function that returns True if the object is exactly an instance of the specified class ; otherwise False """
+""" defines function that returns True if the object is exactly
+an instance of the specified class ; otherwise False """
 
 
-class MyList(list):
-    def print_sorted(self):
-        self.__new_list = self[:]
-        self.__new_list.sort()
-        print(self.__new_list)
+def is_same_class(obj, a_class):
+    """ returns True if the object is exactly an instance
+of the specified class ; otherwise False. """
+    return obj.__class__ == a_class
