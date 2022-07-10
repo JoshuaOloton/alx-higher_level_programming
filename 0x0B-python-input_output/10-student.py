320139ed90526b3d38b14cebe30b41d190928be1
JoshuaOloton <olotonjoshua@gmail.com>
2022-07-10 06:02:40 +0000

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+''' defines a class Student '''
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        ''' class constructor method '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        ''' retrieves dictionary representation of instance '''
+        new_dict = {}
+        if not attrs:
+            return self.__dict__
+        for string in attrs:
+            if string in self.__dict__.keys():
+                new_dict[string] = self.__dict__[string]
+        return new_dict
