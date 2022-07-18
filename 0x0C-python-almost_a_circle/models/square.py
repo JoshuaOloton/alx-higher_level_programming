4311a761777c3d9b57857d81458115bbcd3bbfd7
JoshuaOloton <olotonjoshua@gmail.com>
2022-07-18 16:12:35 +0000

    modified square.py file

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,21 +9,20 @@ class Square(Rectangle):
     ''' square subclass of imported Base Class '''
     def __init__(self, size, x=0, y=0, id=None):
         ''' constructor function '''
-        self.size = size
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
         ''' size getter '''
-        return self.__size
+        return self.width
 
     @size.setter
     def size(self, size):
         ''' size setter '''
-        super().update(size, size)
-        self.__size = size
+        self.width = size
+        self.height = size
 
     def __str__(self):
         ''' str function '''
         return f'[{self.__class__.__name__}] ({self.id}) '\
-            f'{self.x}/{self.y} - {self.__size}'
+            f'{self.x}/{self.y} - {self.width}'
