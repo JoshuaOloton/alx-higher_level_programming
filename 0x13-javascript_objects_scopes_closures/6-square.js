9800645fc13663703a93465cae69c6bd0d61dd2a
Joshua Oloton <olotonjoshua@gmail.com>
2022-09-06 19:02:14 +0100

    fixed task 6 errors

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,13 +2,10 @@
 const OldSqaure = require('./5-square');
 module.exports = class Square extends OldSqaure {
   charPrint (c) {
+    if (c === undefined) c = 'X';
     for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.height; j++) {
-        if (c === undefined) {
-          process.stdout.write('X');
-        } else {
-          process.stdout.write('C');
-        }
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write(c);
       }
       process.stdout.write('\n');
     }
