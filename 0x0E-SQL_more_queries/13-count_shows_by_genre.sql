8c45ab9ebbd4cb83d7f2a13114d85d8d1b459974
Joshua Oloton <olotonjoshua@gmail.com>
2022-09-24 17:00:11 +0100

    fixed task 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -9,5 +9,5 @@ SELECT `tv_genres`.`name` AS `genre`, COUNT(`tv_show_genres`.`show_id`) AS `numb
 FROM `tv_genres`
 LEFT JOIN `tv_show_genres`
 ON `tv_genres`.`id`=`tv_show_genres`.`genre_id`
-GROUP BY `tv_genres`.`name`;
+GROUP BY `tv_genres`.`name`
 ORDER BY `number_of_shows` DESC;
