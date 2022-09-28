685280015da8bb03396bac67cf551cf6cf4cdcf2
Joshua Oloton <olotonjoshua@gmail.com>
2022-09-28 14:35:37 +0100

    added task 2

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
         html = resp.read()
 print("Body response:")
 print(f"\t-type: {type(html)}")
-print(f"\t-content: {type(html)}")
-print(f"\t-utf-8 content: {html.decode()}")
+print(f"\t-content: {html}")
+print(f"\t-utf-8 content: {html.decode('utf-8')}")
