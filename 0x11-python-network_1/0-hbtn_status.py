b81c964d893f70950e63bc575f5818a377b33673
Joshua Oloton <olotonjoshua@gmail.com>
2022-09-28 13:14:24 +0100

    added task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""  script that fetches https://alx-intranet.hbtn.io/status """
+
+if __name__ == "__main__":
+    import urllib.request
+
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as resp:
+        html = resp.read()
+    print(html)
+print("Body response:")
+print(f"\t-type: {type(html)}")
+print(f"\t-content: {type(html)}")
+print(f"\t-utf-8 content: {html.decode()}")
