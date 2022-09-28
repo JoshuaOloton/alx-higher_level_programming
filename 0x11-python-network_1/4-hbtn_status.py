d2e47eeb2743ec26bc865d0cd766051f78e69a3e
Joshua Oloton <olotonjoshua@gmail.com>
2022-09-28 15:37:47 +0100

    added documentation

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-#  fetches https://alx-intranet.hbtn.io/status
-# using requests package
+""" fetches https://alx-intranet.hbtn.io/status
+using requests package """
 
 if __name__ == "__main__":
     import requests
